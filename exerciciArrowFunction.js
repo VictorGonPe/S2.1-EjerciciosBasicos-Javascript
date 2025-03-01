47902081efadbef2f30322cbba2104be06449d58
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-01 11:17:12 +0100

    Arrow Functions

---
+++ exerciciArrowFunction.js
@@ -0,0 +1,49 @@
+//_______________________________Nivell 1____________________________________
+
+//Exercici 1
+
+const suma = (a, b) => a + b;
+
+suma (3,6); // comprobación
+console.log(suma);
+
+//Exercici 2
+
+const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+for (let i = 0; i < 10; i++) { // comprobación
+  console.log(randomNumber());
+}
+
+//Exercici 3
+
+class Person {
+  constructor(name){
+    this.name = name;
+  }
+  greet = () => console.log(`Hola, ${this.name}`);
+}
+
+let user = new Person('Víctor'); // comprobación
+user.greet();
+
+//_______________________________Nivell 2______________________________________
+
+//Exercici 4
+
+const arrayNumbers = [6,3,4,8];
+
+function printNumbers(arrayNum) {
+    for (let i = 0; i < arrayNum.length; i++) {
+      let print = () => console.log(arrayNum[i]);
+      print();
+    } 
+}
+
+printNumbers(arrayNumbers); 
+
+//_______________________________Nivell 3______________________________________
+
+//Exercici 5
+
+setTimeout(() => console.log("Impreso después de 3 segundos"), 3000);
---
+++ exerciciOperadorTernari.js
@@ -0,0 +1,25 @@
+//_______________________________Nivell 1____________________________________
+
+//Exercici 1
+
+function potConduir (edat) {
+    let resultat = edat > 17 ? "Pots conduir" : "No pots conduir";
+    console.log(resultat);
+}
+
+potConduir(15); // comprobación
+potConduir(24);
+
+//Exercici 2
+
+const bigNum = (num1,num2) => {
+    let resultado = num1 > num2 ? "num1 és més gran" : "num2 és més gran";
+    return resultado;
+  }
+  
+  console.log(bigNum(12,9)); // comprobación
+
+  //_______________________________Nivell 2____________________________________
+
+  //Exercici 3
+
