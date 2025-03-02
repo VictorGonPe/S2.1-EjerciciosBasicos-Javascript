c5569047f4af77b594c527973c52c161cf367282
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-02 22:30:17 +0100

    the exercice of ternary operators has ended

--- exerciciOperadorTernari.js
+++ exerciciOperadorTernari.js
@@ -21,5 +21,28 @@ const bigNum = (num1,num2) => {
 
   //_______________________________Nivell 2____________________________________
 
-  //Exercici 3
+  //Exercici 3 A
+  let num = -3
+  num < 0 ? console.log("es negatiu") : num === 0 ? console.log("es Zero") : console.log("es positiu"); 
 
+  //Exercici 3 B
+  function trobarMaxin(a,b,c) {
+    const maxim = (a>b && a>c) ? a : 
+                  (b>a && b>c) ? b : c; 
+    return maxim;
+  }
+    
+  console.log("The highest number is: " + trobarMaxin(12,54,13)); 
+
+   //_______________________________Nivell 4____________________________________
+
+  //Exercici 4
+  let arrayEdades = [21,32,44,34,45,13,37,29];
+
+  function parOImpar (arrayNum) {
+    arrayNum.forEach(num =>
+       num % 2 === 0 ?  console.log(`${num} es par`) : console.log(`${num} es impar`)
+    );
+  }
+
+  parOImpar(arrayEdades);
