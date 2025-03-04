df00e020b03cecd7e5792003e4c65bb49f572658
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-03-04 13:35:43 +0100

    "Call back" exercises were implemented

---
+++ exerciciCallbacks.js
@@ -0,0 +1,56 @@
+//_______________________________Nivell 1____________________________________
+
+//Exercici 1
+
+function procesar(nombre, callback) {
+    console.log(`Hola ${nombre}, ¿tienes hambre?`)
+    callback();
+}
+
+const comer = ()=> console.log('¡Siempre tengo hambre!')
+
+procesar("Victor", comer);
+
+//Exercici 2
+
+function calculadora(num1,num2,callback) {
+    return callback(num1,num2);
+}
+
+const suma = (num1,num2) => num1 + num2;
+
+console.log(`La suma de los números es: ${calculadora(6,3, suma)}`);
+
+//_______________________________Nivell 2____________________________________
+
+//Exercici 3
+
+function esperarYSaludar (nombre,callback) {
+    setTimeout(()=> callback(nombre), 3000);
+}
+
+const usuario = (nombre) => console.log(`Hola ${nombre}`);
+
+esperarYSaludar("Silvia", usuario);
+
+//Exercici 4
+
+let portatiles = ["Apple","Asus","HP","Acer","samsung"];
+
+function procesarElementos(arrayPC,callback) {
+    arrayPC.forEach(elementos => callback(elementos));
+}
+
+const marcas = (elementos) => console.log(`Marcas en venta: ${elementos}`);
+
+procesarElementos(portatiles,marcas);
+
+//_______________________________Nivell 3____________________________________
+
+//Exercici 5
+
+const procesarCadena = (string, callback ) => callback(string.toUpperCase());
+
+const aMayusculas = (string) => console.log(`La cadena transformada es: ${string}`);
+
+procesarCadena("¡hola mundo!", aMayusculas);
